737f5eede795902619d1f2befb6c641ae83d9125
ming <whuzouming@gmail.com>
2013-11-02 14:05:54 +0800

    init

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 #-*- coding:utf-8 -*-
-mysql_profile={'host':'127.0.0.1','port':3306,'user':'root','password':'111111','db':'duckweed'} 
+mysql_profile={'host':'127.0.0.1','port':3306,'user':'root','password':'111111','db':'fabscheduler'} 
 web={'port':8443,'domain':'fe'}
 max_processes=20
 
